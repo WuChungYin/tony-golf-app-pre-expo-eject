7a2d9b3721a48f04bc3351e8b9b36ffb1923eeb2
Chung-Yin Wu <wuchu@oregonstate.edu>
2020-04-16 16:32:50 -0500

    added LoginStackNavigator

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { StyleSheet, Platform, Image, Text, View } from "react-native";
 import { createSwitchNavigator, createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 // import the different screens
-import Loading from "./Loading";
-import SignUp from "./SignUp";
-import Login from "./Login";
-import Main from "./Main";
+import Loading from "./screens/Loading";
+import SignUp from "./screens/SignUp";
+import Login from "./screens/Login";
+import Main from "./screens/Main";
 
 import { decode, encode } from "base-64";
 if (!global.btoa) {
@@ -15,12 +16,16 @@ if (!global.atob) {
   global.atob = decode;
 }
 
+const LoginStackNavigator = createStackNavigator({
+  Login: { screen: Login },
+  SignUp: { screen: SignUp },
+});
+
 // create our app's navigation stack
 const SwitchContainer = createSwitchNavigator(
   {
     Loading,
-    SignUp,
-    Login,
+    LoginStackNavigator,
     Main,
   },
   {
