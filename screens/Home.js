aa9cb482018e532042335be6576fdc7c05f495cd
Chung-Yin Wu <wuchu@oregonstate.edu>
2020-04-17 13:04:52 -0500

    renamed Main.js to Home.js

---
+++ screens/Home.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { StyleSheet, Platform, Image, Text, View, Button } from "react-native";
+
+import Firebase from "../config/Firebase.js";
+
+export default class Home extends React.Component {
+  state = { currentUser: null };
+  componentDidMount() {
+    const { currentUser } = Firebase.auth();
+    this.setState({ currentUser });
+  }
+  handleLogout = () => {
+    Firebase.auth()
+      .signOut()
+      .then(() => {})
+      .catch((error) => {
+        console.log(error);
+      });
+    this.setState({ currentUser: null });
+  };
+  render() {
+    const { currentUser } = this.state;
+    return (
+      <View style={styles.container}>
+        <Text>Hi {currentUser && currentUser.email}!</Text>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
