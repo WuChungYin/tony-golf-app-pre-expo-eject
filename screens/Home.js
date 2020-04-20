3e7ef7ac682fd9cb271273306402da5300c774bf
Chung-Yin Wu <wuchu@oregonstate.edu>
2020-04-20 16:02:07 -0500

    added practice and lesson credits display

--- screens/Home.js
+++ screens/Home.js
@@ -1,28 +1,36 @@
 import React from "react";
 import { StyleSheet, Platform, Image, Text, View, Button } from "react-native";
 
-import Firebase from "../config/Firebase.js";
+import Firebase, { db } from "../config/Firebase.js";
 
 export default class Home extends React.Component {
-  state = { currentUser: null };
+  state = { currentUser: null, lessonCredits: null, practiceCredits: null };
+
   componentDidMount() {
     const { currentUser } = Firebase.auth();
     this.setState({ currentUser });
-  }
-  handleLogout = () => {
-    Firebase.auth()
-      .signOut()
-      .then(() => {})
-      .catch((error) => {
-        console.log(error);
+
+    const uid = currentUser.uid;
+    console.log("user's UID: " + uid);
+
+    db.collection("users")
+      .doc(uid)
+      .get()
+      .then((doc) => {
+        //console.log("Lesson credits: " + doc.data().lessonCredits);
+        //console.log("Practice credits: " + doc.data().practiceCredits);
+        this.setState({ lessonCredits: doc.data().lessonCredits });
+        this.setState({ practiceCredits: doc.data().practiceCredits });
       });
-    this.setState({ currentUser: null });
-  };
+  }
+
   render() {
     const { currentUser } = this.state;
     return (
       <View style={styles.container}>
         <Text>Hi {currentUser && currentUser.email}!</Text>
+        <Text>Lesson Credits: {this.state.lessonCredits}</Text>
+        <Text>Practice Credits: {this.state.practiceCredits}</Text>
       </View>
     );
   }
