4a6cc1131ab4c38a32dac8c6504e7874367ee83c
Chung-Yin Wu <wuchu@oregonstate.edu>
2020-04-21 16:27:30 -0500

    Added buttons and navigation redirecting to Purchase and Reserve screens, as well as a credit check determining whether to allow the user onto the Reserve screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { StyleSheet, Platform, Image, Text, View, Button } from "react-native";
+import {
+  StyleSheet,
+  Platform,
+  Image,
+  Text,
+  View,
+  Button,
+  Alert,
+} from "react-native";
 
 import Firebase, { db } from "../config/Firebase.js";
 
@@ -17,12 +25,56 @@ export default class Home extends React.Component {
       .doc(uid)
       .get()
       .then((doc) => {
-        //console.log("Lesson credits: " + doc.data().lessonCredits);
-        //console.log("Practice credits: " + doc.data().practiceCredits);
         this.setState({ lessonCredits: doc.data().lessonCredits });
         this.setState({ practiceCredits: doc.data().practiceCredits });
       });
   }
+
+  handleLessonCheckAndRedirect = () => {
+    if (this.state.lessonCredits > 0) {
+      this.props.navigation.navigate("Reserve");
+    } else {
+      Alert.alert(
+        "No Lesson Credits",
+        "You have no lesson credits. Would you like to purchase some?",
+        [
+          {
+            text: "No",
+            onPress: () => this.props.navigation.navigate("Home"),
+            style: "cancel",
+          },
+          {
+            text: "Yes",
+            onPress: () => this.props.navigation.navigate("Purchase"),
+          },
+        ],
+        { cancelable: false }
+      );
+    }
+  };
+
+  handlePracticeCheckAndRedirect = () => {
+    if (this.state.practiceCredits > 0) {
+      this.props.navigation.navigate("Reserve");
+    } else {
+      Alert.alert(
+        "No Practice Credits",
+        "You have no practice credits. Would you like to purchase some?",
+        [
+          {
+            text: "No",
+            onPress: () => this.props.navigation.navigate("Home"),
+            style: "cancel",
+          },
+          {
+            text: "Yes",
+            onPress: () => this.props.navigation.navigate("Purchase"),
+          },
+        ],
+        { cancelable: false }
+      );
+    }
+  };
 
   render() {
     const { currentUser } = this.state;
@@ -31,6 +83,18 @@ export default class Home extends React.Component {
         <Text>Hi {currentUser && currentUser.email}!</Text>
         <Text>Lesson Credits: {this.state.lessonCredits}</Text>
         <Text>Practice Credits: {this.state.practiceCredits}</Text>
+        <Button
+          title="Reserve Lesson"
+          onPress={this.handleLessonCheckAndRedirect}
+        />
+        <Button
+          title="Reserve Practice Lane"
+          onPress={this.handlePracticeCheckAndRedirect}
+        />
+        <Button
+          title="Purchase Credits"
+          onPress={() => this.props.navigation.navigate("Purchase")}
+        />
       </View>
     );
   }
