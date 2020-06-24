855f1d9394d946871b193df3bcfb68ee2b5354fa
Chung-Yin Wu <wuchu@oregonstate.edu>
2020-06-24 14:40:36 -0500

    now pulls data from appointments collection in Firebase -- reservations of the current user

--- screens/ViewReservations.js
+++ screens/ViewReservations.js
@@ -1,28 +1,67 @@
 import React from "react";
-import { StyleSheet, Platform, Image, Text, View, Button } from "react-native";
+import {
+  StyleSheet,
+  Platform,
+  Image,
+  Text,
+  View,
+  Button,
+  FlatList,
+  Alert,
+} from "react-native";
 
-import Firebase from "../config/Firebase.js";
+import Firebase, { db } from "../config/Firebase.js";
 
 export default class ViewReservations extends React.Component {
-  state = { currentUser: null };
+  state = { currentUser: null, apptSlotsData: [] };
+
   componentDidMount() {
     const { currentUser } = Firebase.auth();
     this.setState({ currentUser });
+
+    this.unsubscribe = db
+      .collection("appointments")
+      .where("uid", "==", currentUser.uid)
+      .onSnapshot(this.onCollectionUpdate);
   }
-  handleLogout = () => {
-    Firebase.auth()
-      .signOut()
-      .then(() => {})
-      .catch((error) => {
-        console.log(error);
+
+  collectionWillUnMount() {
+    this.unsubscribe();
+  }
+
+  onCollectionUpdate = (querySnapshot) => {
+    const apptSlotsData = [];
+    querySnapshot.forEach((doc) => {
+      const { id, date, time, type, name, credits } = doc.data();
+      apptSlotsData.push({
+        id,
+        date,
+        time,
+        type,
+        name,
+        credits,
       });
-    this.setState({ currentUser: null });
+    });
+    this.setState({ apptSlotsData: apptSlotsData });
+    console.log("appSlotsData = " + JSON.stringify(this.state.apptSlotsData));
   };
+
   render() {
     const { currentUser } = this.state;
     return (
       <View style={styles.container}>
-        <Text>Hi {currentUser && currentUser.email}!</Text>
+        <Text>This is View Reservations Screen</Text>
+        <FlatList
+          data={this.state.apptSlotsData}
+          renderItem={({ item, index }) => (
+            <View>
+              <Text>
+                {item.date} {item.time} {item.name}
+              </Text>
+            </View>
+          )}
+          keyExtractor={(item, index) => index.toString()}
+        />
       </View>
     );
   }
